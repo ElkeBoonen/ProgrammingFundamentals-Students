0fc015110e0289b1e8cd226ac52230d709eaaff2
Elke Boonen <u0116591@thomasmore.be>
2020-12-07 15:38:11 +0100

    les 07/12

---
+++ OO-Words-IMS/Words.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OO_Words_IMS
+{
+    class Words
+    {
+        public string[] WordsArray { get; set; }
+
+        public Words(string[] array)
+        {
+            WordsArray = array;
+        }
+
+
+
+        public string Sort()
+        {
+            Array.Sort(WordsArray);
+            string s = "";
+            foreach (var item in WordsArray)
+            {
+                s += item + " ";
+            }
+            return s.Trim();
+        }
+
+        public string MostOccurrences()
+        {
+            return "";
+        }
+
+        public string Occurrences()
+        {
+            string s = "";
+            foreach (var item in WordsArray)
+            {
+                s += item + " ";
+            }
+            return s.Trim();
+        }
+
+        public string Unique()
+        {
+            return "";
+        }
+
+    }
+    
+}
