69fb0942b4ce2e65d0b71d55e3e8150e8774d23a
Elke Boonen <u0116591@thomasmore.be>
2020-12-07 14:18:04 +0100

    14:17

---
+++ OO-IMS/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OO_IMS
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        private DateTime Birth { get; set; }
+
+        public Person()
+        {
+            Name = "John Doe";
+            Birth = new DateTime(2000,1,1);
+        }
+
+        public Person(string name, DateTime birth)
+        {
+            Name = name;
+            Birth = birth;
+        }
+    }
+}
---
+++ OO-IMS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OO_IMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person elke = new Person("Elke Boonen", new DateTime(1980,4,23));
+            Person john = new Person();
+
+            elke.
+        }
+    }
+}
