68ee686d3bf59435950b0f3df0d2e14e48a86a08
Elke Boonen <u0116591@thomasmore.be>
2020-12-09 10:35:14 +0100

    class 9/12

---
+++ OO-DSPS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OO_DSPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person();
+            Person elke = new Person("Elke", new DateTime(1980, 4, 23));
+
+            Console.WriteLine(elke.Name);
+            elke.Name = "Elke Boonen";
+            Console.WriteLine(elke.Name + " " + elke.Age());
+
+            Console.WriteLine(elke.ToString()); ;
+
+            elke.Print();
+
+            Student bob = new Student("Bob", new DateTime(2000,5,2));
+            Teacher christophe = new Teacher("Christophe", new DateTime(1980, 1, 14));
+
+            List<Person> people = new List<Person>();
+            people.Add(bob);
+            people.Add(elke);
+            people.Add(christophe);
+            people.Add(john);
+
+            foreach (var item in people)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ OO-DSPS/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OO_DSPS
+{
+    class Student : Person
+    {
+        public Student(string name, DateTime birth)
+        {
+            Name = name;
+            Birth = birth;
+        }
+
+        public override string ToString()
+        {
+            return "STUDENT: " + Name + " - " + Birth.ToLongDateString();
+        }
+    }
+}
