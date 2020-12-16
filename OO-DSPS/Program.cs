2b6f3f526b4cf6cda4acdad374d93ac124373602
Elke Boonen <u0116591@thomasmore.be>
2020-12-16 11:34:29 +0100

    class 16/12

--- OO-DSPS/Program.cs
+++ OO-DSPS/Program.cs
@@ -20,12 +20,22 @@ namespace OO_DSPS
 
             Student bob = new Student("Bob", new DateTime(2000,5,2));
             Teacher christophe = new Teacher("Christophe", new DateTime(1980, 1, 14));
+            Teacher tristan = new Teacher("Tristan", new DateTime(1994, 1, 14));
+
+
+            List<Course> courses = new List<Course>();
+
+            Course prog = new Course("Programming", christophe, "C2.10", 3, WeekDays.Wednesday, new Time());
+            courses.Add(prog);
+            courses.Add(new Course("Databases", tristan, "online", 3, WeekDays.Monday, new Time(9, 15)));
+            bob.Courses = courses;
 
             List<Person> people = new List<Person>();
             people.Add(bob);
             people.Add(elke);
             people.Add(christophe);
             people.Add(john);
+            people.Add(tristan);
 
             foreach (var item in people)
             {
--- OO-DSPS/Student.cs
+++ OO-DSPS/Student.cs
@@ -6,15 +6,33 @@ namespace OO_DSPS
 {
     class Student : Person
     {
+        public List<Course> Courses { get; set; }
+
         public Student(string name, DateTime birth)
         {
             Name = name;
             Birth = birth;
         }
 
+        public Student(string name, DateTime birth, List<Course> courses)
+        {
+            Name = name;
+            Birth = birth;
+            Courses = courses;
+        }
+
         public override string ToString()
         {
-            return "STUDENT: " + Name + " - " + Birth.ToLongDateString();
+            string s = "STUDENT: " + Name + " - " + Birth.ToLongDateString();
+            if (Courses != null)
+            {
+                s += "\nList of courses:\n";
+                foreach (var item in Courses)
+                {
+                    s += "\n-" + item;
+                }
+            }
+            return s;
         }
     }
 }
