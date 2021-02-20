770811ad2f17cb89f3b28f98c32cb489c54fb9ef
ElkeBoonen <39384382+ElkeBoonen@users.noreply.github.com>
2021-02-20 16:27:42 +0100

    class 19/2

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.Write("Hello World!");
+            Console.Write("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Elke!");
+
+            string firstname = "Elke";
+            string lastname;
+            lastname = "Boone";
+            Console.WriteLine(firstname);
+            Console.WriteLine(lastname);
+
+            lastname = "Boonen";
+            Console.WriteLine(firstname);
+            Console.WriteLine(lastname);
+
+            Console.WriteLine(firstname + " " +  lastname);
+
+            Console.WriteLine("Tell me your name");
+            firstname = Console.ReadLine();
+            Console.WriteLine("Your name is " + firstname);
+        }
+    }
+}
