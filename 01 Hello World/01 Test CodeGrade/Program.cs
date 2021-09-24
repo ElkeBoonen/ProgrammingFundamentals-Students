d1ce1e306e04e45b5165a27e84fee0af53f11152
Elke Boonen <u0116591@thomasmore.be>
2021-09-24 18:40:02 +0200

    01 Hello World

---
+++ 01 Hello World/01 Test CodeGrade/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_Test_CodeGrade
+{
+    class Program
+    {
+        /*Read the name and surname of a person and say 'Hello' to them!
+
+        input
+        Elke
+        Boonen
+
+        output
+        Hello Elke Boonen!*/
+
+        //dit is ook commentaar
+        static void Main(string[] args)
+        {
+            string voornaam = Console.ReadLine();
+            string famnaam = Console.ReadLine();
+
+            Console.WriteLine($"Hello {voornaam} {famnaam}!");
+        }
+    }
+}
---
+++ 01 Hello World/Hello World - IMS B/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
+            System.Console.WriteLine("BLABLABLA");
+            System.Console.WriteLine("BLABLABLA!");
+
+            string anderenaam = "Elke";
+            Console.WriteLine(anderenaam);
+
+            string name;
+            name = "Elke";
+            Console.WriteLine(name);
+            Console.WriteLine(name);
+            name = "Mies";
+            Console.WriteLine(name);
+            name = "Bob";
+            Console.WriteLine(name);
+
+            Console.Write("Geef je naam in: ");
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("Hello {name}!");
+            Console.WriteLine($"Hello {name}!"); //dollarteken!!!!!
+            Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}
