bbd1beeb05101e87fea7ceb0aeda260cc54ae9be
Elke Boonen <u0116591@thomasmore.be>
2021-10-08 10:14:51 +0200

    les 8/10

---
+++ 03 If structures/Coins - GROEP A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Coins___GROEP_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een bedrag in!");
+            double bedrag = Convert.ToDouble(Console.ReadLine());
+
+            int euro2 = (int)bedrag / 2;
+            Console.WriteLine("2 euro: " + euro2);
+
+            int euro1 = (int)Math.Floor(bedrag % 2); // Math.Floor is hier overbodig
+            Console.WriteLine("1 euro: " + euro1);
+
+            bedrag = Math.Round(bedrag - (2 * euro2) - euro1,2);
+            Console.WriteLine(bedrag);
+
+            int cent50 = (int)(bedrag / 0.50);
+            Console.WriteLine("50 eurocent: " + cent50);
+
+            bedrag -= cent50*0.5; //bedrag = bedrag - (cent50*0.50)
+            bedrag = Math.Round(bedrag, 2);
+
+            int cent20 = (int)(bedrag / 0.20);
+            Console.WriteLine("20 eurocent: " + cent20);
+        }
+    }
+}
---
+++ 03 If structures/If structuren - groep A/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace If_structuren___groep_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef een x in: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Geef een y in: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x < y)
+            {
+                Console.WriteLine("x is kleiner dan y");
+            }
+            else if (x == y)
+            {
+                Console.WriteLine("x is gelijk aan y");
+            }
+            else
+            {
+                Console.WriteLine("x is groter dan y");
+            }
+
+            //kortere schrijfwijze
+            if (x < y) Console.WriteLine("x is kleiner dan y");
+            else if (x == y) Console.WriteLine("x is gelijk aan y");
+            else Console.WriteLine("x is groter dan y");
+
+            //Let op zelfde if-structuur!! anders rare resultaten!
+            if (x < y)
+            {
+                Console.WriteLine("x is kleiner dan y");
+            }
+            if (x == y)
+            {
+                Console.WriteLine("x is gelijk aan y");
+            }
+            else
+            {
+                Console.WriteLine("x is groter dan y");
+            }
+
+
+            //ANDERE SCHRIJFWIJZEN
+            if (x < y)
+            {
+                Console.WriteLine("x is kleiner dan y");
+            }
+            if (x == y)
+            {
+                Console.WriteLine("x is gelijk aan y");
+            }
+            if (x > y)
+            {
+                Console.WriteLine("x is groter dan y");
+            }
+
+            if (x != y)
+            {
+                if (x > y)
+                {
+                    Console.WriteLine("x is groter dan y");
+                }
+                else
+                {
+                    Console.WriteLine("x is kleiner dan y");
+                }
+            }
+            else
+            {
+                Console.WriteLine("x is gelijk aan y");
+            }
+
+            if (x == y)
+            {
+                Console.WriteLine("x is gelijk aan y");
+            }
+            else
+            {
+                if (x > y)
+                {
+                    Console.WriteLine("x is groter dan y");
+                }
+                else
+                {
+                    Console.WriteLine("x is kleiner dan y");
+                }
+            }
+
+
+        }
+    }
+}
