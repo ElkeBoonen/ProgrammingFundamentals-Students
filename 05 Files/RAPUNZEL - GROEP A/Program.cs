81048d7c724dd0402a4538e9a304f6ff6e947329
Elke Boonen <u0116591@thomasmore.be>
2021-10-22 13:46:04 +0200

    les 22/10

---
+++ 05 Files/RAPUNZEL - GROEP A/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RAPUNZEL___GROEP_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hoe vaak komt 'rapunzel' voor in de tekst?
+            StreamReader stream = File.OpenText("Rapunzel.txt");
+            string text = stream.ReadToEnd().ToLower();
+            stream.Close();
+
+            int count = 0;
+            string word = "";
+            foreach (var item in text)
+            {
+                //if ((int)item >= 97 && (int)item <= 122)
+                //if ("azertyuiopqsdfghjklmwxcvbn".Contains(item)) 
+                //if (item == 'a' || item == 'b' || item == 'c' || ...)
+
+                if (Char.IsLetter(item))
+                {
+                    word += item;
+                }
+                else {
+                    if (word == "rapunzel") count++;
+                    word = "";
+                }
+            }
+            Console.WriteLine($"Rapunzel komt {count} keer voor in de tekst!");
+
+            Regex regex = new Regex(@"rapunzel", RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(text);
+            Console.WriteLine($"Rapunzel komt {matches.Count} keer voor in de tekst!");
+         
+
+            regex = new Regex(@"\w*ra\w*", RegexOptions.IgnoreCase);
+            matches = regex.Matches(text);
+            Console.WriteLine($"{matches.Count} woorden met ra!");
+            foreach (var item in matches)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+
+        }
+    }
+}
---
+++ 05 Files/TEST CODEGRADE - GROEP A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace TEST_CODEGRADE___GROEP_A
+{
+    class Program
+    {
+        /*Read in the file Harry Potter and the Sorcerer.txt  
+         * Read in a given number of lines. Write those lines (with their line numbers) to another 
+         * file with the name harry-<nr of lines>.txt
+         * input: 12
+         * output: the textile harry-12.txt (open to see the result!) 
+         */
+
+        static void Main(string[] args)
+        {
+            StreamReader reader = File.OpenText(@"Harry.txt");
+            int count = Convert.ToInt32(Console.ReadLine());
+            StreamWriter writer = File.CreateText("harry-" + count + ".txt");
+
+            for (int i = 0; i < count; i++)
+            {
+                string line = reader.ReadLine();
+                writer.WriteLine(i + ": " + line);
+            }
+
+            reader.Close();
+            writer.Close();
+
+        }
+    }
+}
