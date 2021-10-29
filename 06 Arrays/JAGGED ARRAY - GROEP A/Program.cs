f996bf4ee2ea5d3b5e8e045c8db6b3853683c5f6
Elke Boonen <u0116591@thomasmore.be>
2021-10-29 12:20:53 +0200

    les 29/10

---
+++ 06 Arrays/JAGGED ARRAY - GROEP A/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace JAGGED_ARRAY___GROEP_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[][] klassen = new string[5][];
+            klassen[0] = new string[]{ "jan", "piet", "joris", "korneel"};
+            klassen[1] = new string[] { "karen", "kristel", "kathleen" };
+            klassen[2] = new string[] { "tom", "jerry" };
+            klassen[3] = new string[] { "ed", "edd", "eddy" };
+            klassen[4] = new string[] { "mik", "mak", "mon" };
+
+
+            for (int i = 0; i < klassen.GetLength(0); i++)
+            {
+                for (int j = 0; j < klassen[i].Length; j++)
+                {
+                    Console.Write(klassen[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            foreach (var item in klassen[2])
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\n\nGeef een aantal klassen in: ");
+            int aantal = Convert.ToInt32(Console.ReadLine());
+
+            klassen = new string[aantal][];
+
+            for (int i = 0; i < aantal; i++)
+            {
+                Console.WriteLine($"Geef namen in voor klas {i} gescheiden door spatie");
+                klassen[i] = Console.ReadLine().Split(' ');
+            }
+
+            for (int i = 0; i < klassen.GetLength(0); i++)
+            {
+                for (int j = 0; j < klassen[i].Length; j++)
+                {
+                    Console.Write(klassen[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 06 Arrays/TEST CODEGRADE - GROEP A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TEST_CODEGRADE___GROEP_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program to find maximum and minimum elements in an array. 
+
+                input
+                1 5 8 10 5 6 9 10
+
+                output
+                Maximum is 10
+                Minimum is 1
+            */
+
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+
+            int min = array[0];
+            int max = array[0]; 
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min) min = array[i];
+                if (array[i] > max) max = array[i];
+            }
+
+            Console.WriteLine($"Maximum is {max}");
+            Console.WriteLine($"Minimum is {min}");
+
+            //alterantief is om te sorteren
+            Array.Sort(array);
+            Console.WriteLine($"Maximum is {array[array.Length-1]}");
+            Console.WriteLine($"Minimum is {array[0]}");
+
+
+        }
+    }
+}
