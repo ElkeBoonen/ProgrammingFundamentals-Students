08e6eed2b01503875a3b59c4c3366dbbfd3a5291
Elke Boonen <u0116591@thomasmore.be>
2021-11-09 12:01:03 +0100

    les 9/11

---
+++ 07 Exceptions/07 Test CodeGrade/07 Test CodeGrade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _07_Test_CodeGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.OpenText("donkey.txt").ReadToEnd().ToLower();
+            string[] chars = Console.ReadLine().Split(' ');
+            Regex regex = new Regex(chars[0] + @"\w*" + chars[1]);
+            MatchCollection matches = regex.Matches(text);
+
+            string[] words = new string[matches.Count];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = matches[i].Value;
+            }
+
+            Array.Sort(words);
+            string word="";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (word != words[i])
+                {
+                    Console.Write(words[i] + " ");
+                    word = words[i];
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ 07 Exceptions/EXCEPTIONS - GROEP B/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace EXCEPTIONS___GROEP_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //FOUTAFHANDELING MET IF- STRUCTUREN
+            /*
+            Console.Write("Geef een aantal lijnen in: ");
+            string antwoord = Console.ReadLine();
+            int nr;
+            if (!Int32.TryParse(antwoord, out nr))
+            {
+                Console.WriteLine("Verkeerde input! Aantal lijnen is nu 10!");
+                nr = 10;
+            }
+            
+            Console.Write("Geef een filenaam in: ");
+            string filenaam = Console.ReadLine();
+
+            if (!File.Exists(filenaam))
+            {
+                Console.WriteLine("Verkeerde input! File is nu english.txt!");
+                filenaam = "english.txt";
+            }
+
+            StreamReader stream = File.OpenText(filenaam);
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine(stream.ReadLine());
+            }
+            stream.Close();
+            */
+            try
+            {
+                Console.Write("Geef een aantal lijnen in: ");
+                int nr = Convert.ToInt32(Console.ReadLine()); //kan exceptie geven
+                Console.Write("Geef een filenaam in: ");
+                string filenaam = Console.ReadLine();
+
+                StreamReader stream = File.OpenText(filenaam); //kan exceptie geven
+                for (int i = 0; i < nr; i++)
+                {
+                    Console.WriteLine(stream.ReadLine());
+                }
+                stream.Close();
+            }
+            //excepties van specifiek naar algemeen!!!
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File bestaat niet!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Aantal lijnen was verkeerd ingegeven!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Er liep iets mis: " + e.Message); ;
+                //Console.WriteLine(e.StackTrace);
+            }
+            finally {
+                Console.WriteLine("einde code!");
+            }
+            
+
+        }
+    }
+}
