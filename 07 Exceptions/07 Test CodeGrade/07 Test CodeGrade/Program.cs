5e7df2fee1181115672b83d821ade811b76fb059
Elke Boonen <u0116591@thomasmore.be>
2021-11-09 12:32:05 +0100

    les 9/11

--- 07 Exceptions/07 Test CodeGrade/07 Test CodeGrade/Program.cs
+++ 07 Exceptions/07 Test CodeGrade/07 Test CodeGrade/Program.cs
@@ -9,8 +9,25 @@ namespace _07_Test_CodeGrade
         static void Main(string[] args)
         {
             string text = File.OpenText("donkey.txt").ReadToEnd().ToLower();
-            string[] chars = Console.ReadLine().Split(' ');
-            Regex regex = new Regex(chars[0] + @"\w*" + chars[1]);
+            string[] chars = Console.ReadLine().ToLower().Split(' ');
+
+            //afchecken of karakters alfabetisch zijn of niet
+            //met Char-properties Char.IsLetter(chars[0].ToCharArray()[0])
+            //eventueel omzetten naar char-array --> char[] karakters = Array.ConvertAll(chars, Convert.ToChar);
+            //met ascii tabel https://www.asciitable.com/
+            //met "azertyuiopqsdfghjklmwxcvbn".Contains...
+            //opties genoeg!
+
+            if (!"azertyuiopqsdfghjklmwxcvbn".Contains(chars[0]) ||
+                !"azertyuiopqsdfghjklmwxcvbn".Contains(chars[1]))
+            {
+                Console.WriteLine("crazy input");
+                return; //we stoppen uitvoering!!!
+            }
+
+            Regex regex = new Regex(@"\b" + chars[0] + @"\w*" + chars[1] + @"\b");
+            //andere optie om de regex op te bouwen!!
+            //Regex regex = new Regex($@"\b{chars[0]}\w*{chars[1]}\b");
             MatchCollection matches = regex.Matches(text);
 
             string[] words = new string[matches.Count];
