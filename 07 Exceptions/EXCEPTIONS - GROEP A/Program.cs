3e8e84c5176d940d3ca6c2bd250613b19afebc79
Elke Boonen <u0116591@thomasmore.be>
2021-11-12 10:14:54 +0100

    les 12/11

---
+++ 07 Exceptions/EXCEPTIONS - GROEP A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace EXCEPTIONS___GROEP_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef aantal lijnen in: ");
+            int aantal = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geef een filenaam in: ");
+            string filenaam = Console.ReadLine();
+
+            StreamReader stream = File.OpenText(filenaam);
+            for (int i = 0; i < aantal; i++)
+            {
+                Console.WriteLine(stream.ReadLine());
+            }
+            stream.Close();
+        }
+    }
+}
