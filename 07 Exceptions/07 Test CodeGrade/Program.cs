e5f89563e5ae57f0f15b81c124bc2225507de173
Elke Boonen <u0116591@thomasmore.be>
2021-11-12 11:53:22 +0100

    les 12/11

---
+++ 07 Exceptions/07 Test CodeGrade/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _07_Test_CodeGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.OpenText("donkey.txt").ReadToEnd().ToLower();
+            string[] chars = Console.ReadLine().Split(' ');
+
+            Regex regex = new Regex(@"[a-z]", RegexOptions.IgnoreCase);
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (!regex.IsMatch(chars[i]))
+                {
+                    Console.WriteLine("crazy input");
+                    return;
+                }
+            }
+
+            /*foreach (var item in chars)
+            {
+                if (!"azertyuiopqsdfghjklmwxcvbn".Contains(item))
+                {
+                    Console.WriteLine("crazy input");
+                    return;
+                }
+            }
+
+            char[] karakters = Array.ConvertAll(chars, Convert.ToChar);
+            foreach (char item in karakters)
+            {
+                if (!Char.IsLetter(item))
+                {
+                    Console.WriteLine("crazy input");
+                    return;
+                }
+            }*/
+
+            try
+            {
+                regex = new Regex(chars[0] + @"\w*" + chars[1]);
+
+                MatchCollection matches = regex.Matches(text);
+
+                string[] words = new string[matches.Count];
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    words[i] = matches[i].Value;
+                }
+
+                Array.Sort(words);
+                string word = "";
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (word != words[i])
+                    {
+                        Console.Write(words[i] + " ");
+                        word = words[i];
+                    }
+
+                }
+            }
+            catch {
+                Console.WriteLine("crazy input");
+            }
+
+        }
+    }
+}
--- 07 Exceptions/EXCEPTIONS - GROEP A/Program.cs
+++ 07 Exceptions/EXCEPTIONS - GROEP A/Program.cs
@@ -7,17 +7,64 @@ namespace EXCEPTIONS___GROEP_A
     {
         static void Main(string[] args)
         {
-            Console.Write("Geef aantal lijnen in: ");
-            int aantal = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Geef een filenaam in: ");
-            string filenaam = Console.ReadLine();
+            //EXCEPTION HANDLING MET IF-STRUCTUREN!!!
+            /*Console.Write("Geef aantal lijnen in: ");
+            string antwoord = Console.ReadLine();
+            int aantal;
+            if (Int32.TryParse(antwoord, out aantal) == false)
+            { //het is geen success, oepsie!
+                Console.WriteLine("Verkeerd getal ingegeven!");
+                return;
+            }
 
+            Console.Write("Geef een filenaam in: ");
+            string filenaam = Console.ReadLine(); // kan fout lopen
+            if (!File.Exists(filenaam)) //zelfde als File.Exists(filenaam)==false
+            {
+                Console.WriteLine("File bestaat niet!");
+                return;
+            }
             StreamReader stream = File.OpenText(filenaam);
             for (int i = 0; i < aantal; i++)
             {
                 Console.WriteLine(stream.ReadLine());
             }
             stream.Close();
+            */
+
+            try
+            {
+                Console.Write("Geef aantal lijnen in: ");
+                int aantal = Convert.ToInt32(Console.ReadLine()); //kan fout lopen
+                Console.Write("Geef een filenaam in: ");
+                string filenaam = Console.ReadLine(); // kan fout lopen
+
+                StreamReader stream = File.OpenText(filenaam);
+                for (int i = 0; i < aantal; i++)
+                {
+                    Console.WriteLine(stream.ReadLine());
+                }
+                stream.Close();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("U gaf een verkeerd getal in!");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Deze file bestaat niet!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("oepsie! Er liep iets verkeerd!");
+                Console.WriteLine(e.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("The end!");
+            }
+            
+           
         }
     }
 }
