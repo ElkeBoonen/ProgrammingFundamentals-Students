2a7ade68579f99d8e812a70a497290aeb8075a16
Elke Boonen <u0116591@thomasmore.be>
2021-11-26 12:23:26 +0100

    les 26/11

---
+++ 08 Lists & dictionaries/DICTIONARY - GROEP A/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DICTIONARY___GROEP_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, string> fruit = new Dictionary<char, string>();
+
+            fruit.Add('a', "appel");
+            fruit.Add('k', "kiwi");
+            //fruit.Add('a', "aardbei"); --> duplicate key, geeft error!
+            fruit['a'] = "aardbei";
+            fruit['c'] = "citroen";
+            fruit['A'] = "appel";
+            fruit['@'] = "appel";
+
+            Console.WriteLine("FRUIT OVERLOPEN");
+            foreach (var item in fruit) //uit item key & value halen
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+            Console.WriteLine("FRUIT VALUES");
+            foreach (var item in fruit.Values) //enkel values
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("FRUIT KEYS");
+            foreach (var item in fruit.Keys) //enkel keys
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("REMOVE a");
+            fruit.Remove('a');
+            foreach (var item in fruit) //item als key/value afdrukken
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("REMOVE appels");
+            //alle appels eruit halen
+            foreach (var item in fruit)
+            {
+                if (item.Value == "appel")
+                {
+                    fruit.Remove(item.Key);
+                }
+            }
+            foreach (var item in fruit) //item als key/value afdrukken
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("appel en banaan toevoegen");
+            fruit['a'] = "appel";
+            fruit['b'] = "banaan";
+            foreach (var item in fruit) //item als key/value afdrukken
+            {
+                Console.WriteLine(item);
+            }
+
+            char[] keys = fruit.Keys.ToArray(); ;
+            Array.Sort(keys);
+            foreach (var key in keys)
+            {
+                Console.WriteLine(key + ": " + fruit[key]);
+            }
+
+            Console.WriteLine("value appel? " + fruit.ContainsValue("appel"));
+            Console.WriteLine("key p? " + fruit.ContainsKey('p'));
+
+            for (int i = 0; i < fruit.Count; i++)
+            {
+                Console.WriteLine($"{i} --> {fruit.ElementAt(i)}");
+            }
+        }
+    }
+}
---
+++ 08 Lists & dictionaries/List - GROEP B/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List___GROEP_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine("Elementen toevoegen");
+
+            list.Add(10);
+            list.Add(15);
+            list.Add(23);
+            list.Insert(0, 4);
+            list.Insert(1, 8);
+            list.Add(10);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            list.RemoveAt(1);
+            list.Remove(10); //eerste 10 wordt verwijderd!
+            Console.WriteLine("\nElementen verwijderen");
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nBevat 10? " + list.Contains(10));
+            Console.WriteLine("Bevat 8? " + list.Contains(8));
+            Console.WriteLine("Bevat " + list.Count + " elementen");
+            Console.WriteLine("Index van 23: " + list.IndexOf(23));
+            Console.WriteLine("Index van 8: " + list.IndexOf(8));
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            List<double> listdouble = list.ConvertAll(Convert.ToDouble);
+            listdouble.Add(5.6);
+            listdouble.AddRange(new double[]{ 7.1, 7.8, 1 });
+
+            foreach (var item in listdouble)
+            {
+                Console.Write(item + " ");
+            }
+
+            string[] array = { "jan", "piet", "joris", "korneel" };
+            List<string> liststring = array.ToList();
+            liststring.Add("marie");
+
+        }
+    }
+}
