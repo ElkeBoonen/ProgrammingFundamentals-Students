3db49b1c52254d762cd849f099c4c4282c98d01c
Elke Boonen <u0116591@thomasmore.be>
2021-11-30 10:06:50 +0100

    les 30/11

--- 09 Methods/METHODS - GROEP B/Program.cs
+++ 09 Methods/METHODS - GROEP B/Program.cs
@@ -5,9 +5,7 @@ namespace METHODS___GROEP_B
 {
     class Program
     {
-        static int[] array = { 1, 3, 1, 4, 6, 2, 3, 3, 3, 9, 7, 8, 6, 2, 5 };
-
-        static void Print()
+        static void Print(int[] array)
         {
             foreach (var item in array)
             {
@@ -16,19 +14,9 @@ namespace METHODS___GROEP_B
             Console.WriteLine();
         }
 
-        static void Main(string[] args)
-        {
-            /*
-             * Als we werken zonder static bij methode definitie 
-             * Program pr = new Program();
-             * pr.Print();
-             */
-
-            Print();
-
+        static int MeestVoorkomend(int[] array)
+        {            
             //tellen van zelfde items in array
-           
-
             Dictionary<int, int> dictionary = new Dictionary<int, int>();
             foreach (var item in array)
             {
@@ -47,15 +35,37 @@ namespace METHODS___GROEP_B
             int key = 0;
             foreach (var item in dictionary)
             {
-                Console.WriteLine(item);
+                //Console.WriteLine(item);
                 if (item.Value > meestvoorkomend)
                 {
                     key = item.Key;
                     meestvoorkomend = item.Value;
                 }
             }
+            return key;
+        }
 
-            Console.WriteLine($"{key} komt het meeste voor!");
+        static void Main(string[] args)
+        {
+            /*
+             * Als we werken zonder static bij methode definitie 
+             * Program pr = new Program();
+             * pr.Print();
+             */
+
+            int[] array1 = { 1, 3, 1, 4, 6, 2, 3, 3, 3, 9, 7, 8, 6, 2, 5 };
+            int[] array2 = { 1, 2, 3, 4, 5 };
+
+            Print(array1);
+            Array.Sort(array1);
+            Print(array1);
+
+            Print(array2);
+            Array.Reverse(array2);
+            Print(array2);
+
+            int waarde = MeestVoorkomend(array1);
+            Console.WriteLine($"{waarde} komt het meeste voor in array1!");
         }
     }
 }
