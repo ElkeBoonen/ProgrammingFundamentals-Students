f28221d665381b6276b340062026c4d6b3cbd198
Elke Boonen <u0116591@thomasmore.be>
2021-12-03 12:23:08 +0100

    les 3/12

--- 09 Methods/PARAMETERS - GROEP A/Program.cs
+++ 09 Methods/PARAMETERS - GROEP A/Program.cs
@@ -18,7 +18,13 @@ namespace PARAMETERS___GROEP_A
             Console.WriteLine(" --> in methode voor wijziging: " + a);
             a *= 100;
             Console.WriteLine(" --> in methode na wijziging: " + a);
+        }
 
+        static void WijzigWaarde(ref int a)
+        {
+            Console.WriteLine(" --> in methode REF voor wijziging: " + a);
+            a *= 100;
+            Console.WriteLine(" --> in methode REF na wijziging: " + a);
         }
 
         static void WijzigWaarde(int[] array)
@@ -32,6 +38,8 @@ namespace PARAMETERS___GROEP_A
             Console.Write(" --> in methode na wijziging: ");
             Print(array);
         }
+
+        
 
         static void Main(string[] args)
         {
@@ -47,6 +55,41 @@ namespace PARAMETERS___GROEP_A
             Console.Write("in main na methode: ");
             Print(array);
 
+            Console.WriteLine("in main voor REF methode: " + variabele);
+            WijzigWaarde(ref variabele);
+            Console.WriteLine("in main na REF methode: " + variabele);
+
+            Console.WriteLine();
+
+            int a = 3, b = 10;
+            Console.WriteLine($"a: {a} - b: {b}");
+            WisselOmRef(ref a, ref b);
+            Console.WriteLine($"a: {a} - b: {b}");
+      
+            Console.WriteLine($"a: {a} - b: {b}");
+            WisselOmOut(out a, out b);
+            Console.WriteLine($"a: {a} - b: {b}");
+
+            //mooiste voorbeeld van 'wanneer out gebruiken'
+            int getal;
+            bool gelukt = Int32.TryParse("67", out getal);
         }
+
+        static void WisselOmRef(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        static void WisselOmOut(out int a, out int b)
+        {
+            a = 300;
+            b = 20;
+            int c = a;
+            a = b;
+            b = c;
+        }
+
     }
 }
