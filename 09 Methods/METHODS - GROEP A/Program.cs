60f083ded7cb8694d207ad0f55e46695e3c1f149
Elke Boonen <u0116591@thomasmore.be>
2021-12-03 11:14:01 +0100

    les 3/12

---
+++ 09 Methods/METHODS - GROEP A/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace METHODS___GROEP_A
+{
+    class Program
+    {
+        static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Print(double[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Print(string[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        static int MeestVoorkomend(int[] array)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            foreach (var item in array)
+            {
+                if (!dictionary.ContainsKey(item))
+                {
+                    dictionary.Add(item, 1);
+                }
+                else
+                {
+                    dictionary[item] += 1;
+                }
+            }
+            int key = 0;
+            int meeste = 0;
+            foreach (var item in dictionary)
+            {
+                if (item.Value > meeste)
+                {
+                    meeste = item.Value;
+                    key = item.Key;
+                }
+            }
+            return key;
+        }
+
+        static int[] RandomVullen(int lengte = 10)
+        {
+            Random rd = new Random();
+            int[] array = new int[lengte];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rd.Next(1, 10);
+            }
+            return array;
+            //Array.Sort(array); --> dode code!!! Nooit uitgevoerd!!!!
+        }
+
+        static int[] RandomVullen(int min, int max)
+        {
+            Random rd = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rd.Next(min, max);
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            /*zonder static voor Print, moet je dit doen: 
+             * Program pr = new Program();
+             * pr.Print();
+             */
+
+            int[] array1 = { 1, 2, 6, 8, 6, 2, 3, 9, 8, 1, 1, 4, 8, 8, 5, 8, 8, 8, 9 };
+            Print(array1);
+            Console.WriteLine($"{MeestVoorkomend(array1)} komt het meeste voor!");
+
+            int[] array2 = RandomVullen(10,10);
+            Print(array2);
+            int meest = MeestVoorkomend(array2);
+            Console.WriteLine($"{meest} komt het meeste voor!");
+
+            int[] array5 = RandomVullen(100);
+            Print(array5);
+            Console.WriteLine($"{MeestVoorkomend(array5)} komt het meeste voor!");
+
+            int[] array6 = RandomVullen(); //optionele parameter!
+            Print(array6);
+            Console.WriteLine($"{MeestVoorkomend(array6)} komt het meeste voor!");
+
+            int[] array7 = RandomVullen(-19,11);
+            Print(array7);
+            Console.WriteLine($"{MeestVoorkomend(array7)} komt het meeste voor!");
+
+            double[] array3 = { 4.7, 5.1, 9.0 };
+            Print(array3);
+            string[] array4 = { "appel", "peer", "banaan", "kiwi" };
+            Print(array4);
+
+        }
+    }
+}
---
+++ 09 Methods/PARAMETERS - GROEP A/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PARAMETERS___GROEP_A
+{
+    class Program
+    {
+        static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void WijzigWaarde(int a)
+        {
+            Console.WriteLine(" --> in methode voor wijziging: " + a);
+            a *= 100;
+            Console.WriteLine(" --> in methode na wijziging: " + a);
+
+        }
+
+        static void WijzigWaarde(int[] array)
+        {
+            Console.Write(" --> in methode voor wijziging: ");
+            Print(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= 100;
+            }
+            Console.Write(" --> in methode na wijziging: ");
+            Print(array);
+        }
+
+        static void Main(string[] args)
+        {
+            int variabele = 5;
+            Console.WriteLine("in main voor methode: " + variabele);
+            WijzigWaarde(variabele);
+            Console.WriteLine("in main na methode: " + variabele);
+
+            int[] array = { 1, 2, 3 };
+            Console.Write("in main voor methode: ");
+            Print(array);
+            WijzigWaarde(array);
+            Console.Write("in main na methode: ");
+            Print(array);
+
+        }
+    }
+}
