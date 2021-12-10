001a22b4621b5a3c836dd8e3b2382fca1fb0813c
Elke Boonen <u0116591@thomasmore.be>
2021-12-10 10:17:21 +0100

    les 10/12

---
+++ 10 OO/PERSON - GROEP A/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PERSON___GROEP_A
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+    }
+}
---
+++ 10 OO/PERSON - GROEP A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PERSON___GROEP_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
