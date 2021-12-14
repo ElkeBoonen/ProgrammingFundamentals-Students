eee5601f7da810c5a0e2f25a1def8eb1edcbcd93
Elke Boonen <u0116591@thomasmore.be>
2021-12-14 09:29:49 +0100

    les 14/12

---
+++ 10 OO/Bankaccount - GROEP B/Bankrekening.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankaccount___GROEP_B
+{
+    class Bankrekening
+    {
+        public double Saldo { get; set; }
+        public double Intrest { get; set; }
+        public string Eigenaar { get; set; }
+
+        public Bankrekening(double saldo, double intrest, string eigenaar)
+        {
+            Saldo = saldo;
+            Intrest = intrest;
+            Eigenaar = eigenaar;
+            Transactie("OPENING", saldo);
+        }
+
+        public double Storten(double geld)
+        {
+            Saldo += geld;
+            Transactie("STORTING", geld);
+            return Saldo;
+        }
+
+        public double Afhalen(double geld)
+        {
+            if (geld > 2500) 
+            {
+                throw new Exception("Limiet overschreden bij afhaling!");
+            }
+            if (geld > Saldo)
+            {
+                throw new Exception("U kan niet onder 0 gaan!");
+            }
+            Saldo -= geld;
+            Transactie("AFHALING", geld);
+            return Saldo;
+        }
+
+        private void Transactie(string transactie, double geld)
+        {
+            StreamWriter stream = new StreamWriter(Eigenaar + "_transacties.txt", true);
+            stream.WriteLine(DateTime.Now + " " + transactie + " " + geld + " nieuw saldo = " + Saldo);
+            stream.Close();
+        }
+
+        public override string ToString()
+        {
+            string s = "REKENING VAN " + Eigenaar + "\n";
+            StreamReader stream = new StreamReader(Eigenaar + "_transacties.txt");
+            s += stream.ReadToEnd();
+            return s;
+        }
+    }
+}
---
+++ 10 OO/Bankaccount - GROEP B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bankaccount___GROEP_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
