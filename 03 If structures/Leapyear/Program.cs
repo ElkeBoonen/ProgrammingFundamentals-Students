0ca65f5a3a4efbb32b73439573c4f764c104e72d
Elke Boonen <u0116591@thomasmore.be>
2022-10-07 09:53:53 +0200

    IMS - les 2, deel 2

---
+++ 03 If structures/Leapyear/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Leapyear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            string answer = Console.ReadLine();
+
+            int year;
+            if (!Int32.TryParse(answer, out year))
+            {
+                Console.WriteLine("Crazy input!");
+                return; //stopt de uitvoering!
+            }
+
+            //makkelijkste manier om schrikkeljaar te vinden
+            Console.WriteLine($"{year}: {DateTime.IsLeapYear(year)}");
+
+            //maar nu is het aan jullie!
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        Console.WriteLine("Leap year!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a leap year!");
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("Leap year!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a leap year!");
+            }
+
+
+        }
+    }
+}
