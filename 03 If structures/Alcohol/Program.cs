35d1a5f9d1a5faca3127aa73dba79769334eca70
Elke Boonen <u0116591@thomasmore.be>
2022-10-07 09:23:56 +0200

    IMS - les 2

---
+++ 03 If structures/Alcohol/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Alcohol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef uw geboortejaar in: ");
+            string answer = Console.ReadLine();
+
+            int year;
+            bool converted = Int32.TryParse(answer, out year);
+
+            int age;
+
+            if (converted) //converted==true
+            {
+                age = DateTime.Now.Year - year;
+                if (age < 16)
+                {
+                    Console.WriteLine("Jammer, je mag niets!");
+                }
+                else if (age >= 16 && age < 18)
+                {
+                    Console.WriteLine("Je mag al een biertje of een wijntje drinken!");
+                }
+                else
+                {
+                    Console.WriteLine("Je mag loos gaan! Hoera! ;)");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Verkeerde ingave. Duidelijk niet bekwaam genoeg om alcohol te drinken!");
+            }
+
+            ///////////////////////////
+            ///////andere versie!/////
+            //////////////////////////
+            if (!converted)
+            {
+                Console.WriteLine("Verkeerde ingave. Duidelijk niet bekwaam genoeg om alcohol te drinken!");
+                return; //return stopt een methode! dus programma stopt hier!
+                        //break stopt een structuur
+            }
+
+            age = DateTime.Now.Year - year;
+            if (age < 16)
+            {
+                Console.WriteLine("Jammer, je mag niets!");
+            }
+            else if (age >= 16 && age < 18)
+            {
+                Console.WriteLine("Je mag al een biertje of een wijntje drinken!");
+            }
+            else
+            {
+                Console.WriteLine("Je mag loos gaan! Hoera! ;)");
+            }
+
+        }
+    }
+}
---
+++ 03 If structures/If-structures 2 - IMS/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace If_structures_2___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int weekday = (int)DateTime.Now.DayOfWeek;
+            Console.WriteLine(weekday);
+
+            if (weekday == 1)
+            {
+                Console.WriteLine("Monday");
+            }
+            else if (weekday == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            else if (weekday == 3)
+            {
+                Console.WriteLine("Wednesday");
+            }
+            else if (weekday == 4)
+            {
+                Console.WriteLine("Thursday");
+            }
+            else if (weekday == 5)
+            {
+                Console.WriteLine("Friday");
+            }
+            else 
+            {
+                Console.WriteLine("Hooray! Weekend!");
+            }
+
+            switch (weekday)
+            {
+                case 1: 
+                    Console.WriteLine("Monday");
+                    break;
+                case 2: Console.WriteLine("Tuesday");
+                    break;
+                case 3: Console.WriteLine("Wednesday");break;
+                case 4: Console.WriteLine("Thursday");break;
+                case 5: Console.WriteLine("Friday");break;
+                default: 
+                    Console.WriteLine("Hooray! Weekend!");
+                    break;
+            }
+        }
+    }
+}
