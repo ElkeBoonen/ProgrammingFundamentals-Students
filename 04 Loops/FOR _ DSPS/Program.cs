6c713c66aa8cb8cfd83aadd761fcd564661d71e8
Elke Boonen <u0116591@thomasmore.be>
2022-10-10 12:42:11 +0200

    10/10 DSPS

---
+++ 04 Loops/FOR _ DSPS/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace FOR___DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //idiot version
+            Console.WriteLine($"{number} * 1 = {number * 1}");
+            Console.WriteLine($"{number} * 2 = {number * 2}");
+            Console.WriteLine($"{number} * 3 = {number * 3}");
+            Console.WriteLine($"{number} * 4 = {number * 4}");
+
+            //better version
+            Console.WriteLine("\nFOR LOOP i=0 i <= 10");
+            for (int i = 0; i <= 10; i++) //same condition -> i < 11
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+                //Console.WriteLine(number + " * " + i + " = " + result);
+            }
+
+            Console.WriteLine("\nFOR LOOP i=1 i <= 10");
+            for (int i = 1; i <= 10; i++) //same condition -> i < 11
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+                //Console.WriteLine(number + " * " + i + " = " + result);
+            }
+
+            Console.WriteLine("\nFOR LOOP i=0 i <= 10 stepsize 2");
+            for (int i = 0; i <= 10; i+=2) 
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+            }
+
+            Console.WriteLine("\nFOR LOOP i=0 i <= 10 stepsize power of 2");
+            //BE CAREFUL --> INFINITE LOOP, START FROM 1
+            for (int i = 1; i <= 20; i *= 2)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+            }
+
+            Console.WriteLine("\nFOR LOOP i=10 i >= 0 i--");
+            //NO OUTPUT  --> for (int i = 10; i <= 0; i--)
+            //for (int i = 10; i >= 0; i++) 'inifite loop', but it will stop when result is too big for an integer --> overflow exception
+            for (int i = 10; i >= 0; i--)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+            }
+
+            //PRINT 3 6 9 12 15 18 21 14 etc...
+            Console.WriteLine("\nVERSION 1: sequence 3 6 9 12 15 18 ...");
+            for (int i = 1; i <= 33; i++)
+            {
+                int result = i * 3;
+                Console.Write(result + " ");
+            }
+
+            Console.WriteLine("\nVERSION 2: sequence 3 6 9 12 15 18 ...");
+            for (int i = 3; i <= 100; i+=3)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\nVERSION 3: sequence 3 6 9 12 15 18 ...");
+            for (int i = 3; i <= 100; i++)
+            {
+                if (i%3==0) Console.Write(i + " ");
+            }
+
+            Console.Write("\n\nEnter x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nTABLE OF j's");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nTABLE OF stars");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nTABLE OF i's");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+            /* x = 5
+             1
+             2 2
+             3 3 3
+             4 4 4 4
+             5 5 5 5 5
+
+             x = 3
+             1
+             2 2 
+             3 3 3
+             */
+
+            Console.WriteLine("\nPYRAMID");
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04 Loops/Months - DSPS/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Months___DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a month");
+            string answer = Console.ReadLine();
+            int month;
+
+            //try parse input --> check if input is valid!!!
+            if (!Int32.TryParse(answer, out month))
+            {
+                Console.WriteLine("crazy input!");
+                return;
+            }
+
+            //check if current year is leap year, store result in boolean
+            //https://www.softwareideas.net/a/1642/leap-year-check-flowchart-
+            int year = DateTime.Now.Year;
+            bool leapyear = false;
+
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        leapyear = true;
+                    }
+                }
+                else
+                {
+                    leapyear = true;
+                }
+            }
+
+            //output number of days in a month
+            if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                Console.WriteLine("30 days");
+            }
+            else if (month == 2)
+            {
+                //DateTime.IsLeapYear(year)
+                if (leapyear) //same --> (leapyear == true)
+                {
+                    Console.WriteLine("29 days");
+                }
+                else
+                {
+                    Console.WriteLine("28 days");
+                }
+            }
+            else
+            {
+                Console.WriteLine("31 days");
+            }
+
+
+            //version 2
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+                Console.WriteLine("31 days");
+            }
+            else if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                Console.WriteLine("30 days");
+            }
+            else if (leapyear)
+            {
+                Console.WriteLine("29 days");
+            }
+            else
+            {
+                Console.WriteLine("28 days");
+            }
+            
+            //version 3
+            switch (month)
+            {
+                case 4:
+                case 6:
+                case 9:
+                case 11: Console.WriteLine("30 days"); break;
+                case 2:
+                    if (leapyear) //same --> (leapyear == true)
+                    {
+                        Console.WriteLine("29 days");
+                    }
+                    else
+                    {
+                        Console.WriteLine("28 days");
+                    }
+                    break;
+                default: Console.WriteLine("31 days"); break;
+            }
+        }
+    }
+}
