14c5a318f6109871d82ca4ce38626f097464ee9d
Elke Boonen <u0116591@thomasmore.be>
2022-10-17 10:19:18 +0200

    IMS 17/10

---
+++ 05 Files/FILES - IMS/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+
+namespace FILES___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //file wordt gemaakt in BIN-folder --> <projectfolder>\bin\Debug\netcoreapp3.1
+            StreamWriter stream = File.CreateText("file.txt");
+            stream.WriteLine("Voila, hier ben ik!");
+            stream.WriteLine("Nog een regeltje in de file!");
+            stream.Close();
+
+            //file maken op de C-schijf
+            stream = File.CreateText(@"C:\test\file.txt");
+            stream.WriteLine("Voila, hier ben ik!");
+            stream.WriteLine("Nog een regeltje in de file!");
+            stream.Close();
+
+            //file maken op de desktop
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),"file.txt");
+            stream = File.CreateText(path);
+            stream.WriteLine("Voila, hier ben ik!");
+            stream.WriteLine("Nog een regeltje in de file!");
+            stream.Close();
+
+            //CreateText zal altijd overschrijven
+            //AppendText zal toevoegen
+            if (File.Exists(path))
+            {
+                stream = File.AppendText(path);
+                stream.WriteLine("Voila, hier ben ik!");
+                stream.WriteLine("Nog een regeltje in de file!");
+                stream.Close();
+            }
+
+            //file verwijderen
+            if (File.Exists(@"C:\test\file.txt"))
+            {
+                File.Delete(@"C:\test\file.txt");
+            }
+
+            StreamReader input = File.OpenText("Rapunzel.txt");
+            string text = input.ReadToEnd(); //File.ReadAllText
+            input.Close(); // altijd file sluiten!!!!!
+            Console.WriteLine(text);
+
+            //lezen lijn per lijn!
+            input = File.OpenText("Rapunzel.txt");
+            while (!input.EndOfStream)
+            { 
+                Console.WriteLine("-->" + input.ReadLine());
+            }
+            input.Close();
+
+            //lezen char per char
+            input = File.OpenText("Rapunzel.txt");
+            while (!input.EndOfStream)
+            {
+                Console.Write((char)input.Read()+" "); //ASCII waarden, casten naar char
+            }
+            input.Close();
+
+            //foreach gebruiken als je over reeks of collectie wilt lopen!!!
+            foreach (string item in File.ReadLines("Rapunzel.txt"))
+            {
+                Console.WriteLine("**** " + item);
+            }
+
+            foreach (char item in File.ReadAllText("Rapunzel.txt"))
+            {
+                Console.Write(item + " ");
+            }
+
+            foreach (char karakter in File.ReadAllText("Rapunzel.txt"))
+            {
+                Console.Write(karakter);
+            }
+        }
+    }
+}
---
+++ 05 Files/RAPUNZEL - IMS/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO; //altijd als je met files werkt System.IO importeren!!!
+
+namespace RAPUNZEL___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("Rapunzel.txt");
+
+            //juist dezelfde methode!
+            //StreamReader sr = File.OpenText("Rapunzel.txt");
+            //string text = sr.ReadToEnd();
+            //sr.Close();
+
+            Console.WriteLine("Aantal karakters: " + text.Length);
+
+            int count = 0;
+            //karakters checken! --> FOR-LOOP
+            for (int i = 0; i < text.Length; i++)
+            {
+                //if ("aA".Contains(text[i])
+                if (text[i] == 'a' || text[i] == 'A')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Aantal a's: " + count);
+
+            count = 0;
+            //karakters checken! --> FOREACH
+            //zet volledige tekst om naar kleine letters!
+            foreach (var item in text.ToLower())
+            {
+                if (item == 'a')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Aantal a's: " + count);
+
+            count = 0;
+            //karakters checken! --> WHILE
+            StreamReader sr = File.OpenText("Rapunzel.txt");
+            while (!sr.EndOfStream)
+            {
+                int c = sr.Read();
+                if (c==65 || c==97)
+                {
+                    count++;
+                }
+            }
+            sr.Close();
+            Console.WriteLine("Aantal a's: " + count);
+
+
+
+        }
+    }
+}
