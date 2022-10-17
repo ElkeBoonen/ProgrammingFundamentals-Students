b1dd0f2258cffb123279a3a442b28b94b88058a4
Elke Boonen <u0116591@thomasmore.be>
2022-10-17 12:36:08 +0200

    DSPS 17/10

--- 05 Files/FILES - READING - DSPS/Program.cs
+++ 05 Files/FILES - READING - DSPS/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace FILES___READING___DSPS
 {
@@ -11,6 +12,35 @@ namespace FILES___READING___DSPS
             string text = reader.ReadToEnd();
             reader.Close();
             Console.WriteLine(text);
+
+            reader = File.OpenText("Rapunzel.txt");
+            while (!reader.EndOfStream)
+            {
+                //string line = reader.ReadLine();
+                //Console.WriteLine(line);
+                Console.WriteLine("-->" + reader.ReadLine());
+            }
+            reader.Close();
+
+            reader = File.OpenText("Rapunzel.txt");
+            while (!reader.EndOfStream)
+            {
+                Console.Write((char)reader.Read() + " ");
+            }
+            reader.Close();
+
+            //foreach - loop all lines
+            foreach (string item in File.ReadLines("Rapunzel.txt"))
+            {
+                Console.WriteLine("-->" + item);
+            }
+
+            //foreach - loop all characters
+            foreach (char c in File.ReadAllText("Rapunzel.txt"))
+            {
+                Console.Write(c + " ");
+            }
+
         }
     }
 }
---
+++ 05 Files/RAPUNZEL - DSPS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace RAPUNZEL___DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader sr = File.OpenText("Rapunzel.txt");
+            //string text = sr.ReadToEnd();
+            //sr.Close();
+
+            string text = File.ReadAllText("Rapunzel.txt");
+            Console.WriteLine("# chars: " + text.Length);
+
+            Console.WriteLine("# lines: " + File.ReadAllLines("Rapunzel.txt").Length);
+
+            //reading in all lines with a WHILE!
+            StreamReader sr = File.OpenText("Rapunzel.txt");
+            int lines = 0;
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine();
+                lines++;
+            }
+            sr.Close();
+            Console.WriteLine("# lines: " + lines);
+
+            //count the A/a's! 
+            //while
+            //foreach
+            //for
+
+        }
+    }
+}
