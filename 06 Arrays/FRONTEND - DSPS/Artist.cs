c90177cb9b418d1a16d6352b892ff94529ad4640
Elke Boonen <u0116591@thomasmore.be>
2022-10-28 12:09:17 +0200

    DSPS - 28/10

---
+++ 06 Arrays/FRONTEND - DSPS/Artist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRONTEND___DSPS
+{
+    class Artist
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Artist(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{id} - {name}";
+        }
+    }
+}
