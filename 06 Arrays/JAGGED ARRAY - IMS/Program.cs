bd956ac4cd71128db1a296c66bb9b7cddb5f86a3
Elke Boonen <u0116591@thomasmore.be>
2022-10-28 10:26:47 +0200

    IMS 28/10

---
+++ 06 Arrays/JAGGED ARRAY - IMS/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace JAGGED_ARRAY___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /***********************************************/
+            /*************** JAGGED AANMAKEN ***************/
+            /***********************************************/
+
+            int[][] ints = new int[2][];
+            ints[0] = new int[4];
+            ints[1] = new int[3];
+
+            //0 0 0 0
+            //0 0 0
+
+            string[][] strings = {
+                    new string[]{ "jan","piet","joris","korneel" },
+                    new string[] { "eva","marie","ella","kim"} 
+            };
+
+            /***********************************************/
+            /*************** JAGGED ELEMENTEN **************/
+            /***********************************************/
+
+            //0 0 0 5
+            //0 2 0
+
+            ints[0][3] = 5;
+            ints[1][1] = 2;
+
+            Console.WriteLine(strings[1][2]);
+            strings[1][2] = "gloria";
+            Console.WriteLine(strings[1][2]);
+
+            /***********************************************/
+            /*************** JAGGED OVERLOPEN **************/
+            /***********************************************/
+
+            for (int i = 0; i < ints.GetLength(0); i++)
+            {
+                for (int j = 0; j < ints[i].Length; j++)
+                {
+                    Console.Write(ints[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            foreach (var item in ints)
+            {
+                foreach (var element in item)
+                {
+                    sum += element;
+                }
+            }
+            Console.WriteLine("De som: " + sum);
+        }
+    }
+}
---
+++ 06 Arrays/VOETBAL - IMS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VOETBAL___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] voetbal = new string[3][];
+            for (int i = 0; i < voetbal.GetLength(0); i++)
+            {
+                Console.Write("Geef spelers gescheiden door een spatie: ");
+                voetbal[i] = Console.ReadLine().Split(' ');
+            }
+
+            Console.WriteLine($"Formatie: {voetbal[0].Length} - {voetbal[1].Length} - {voetbal[2].Length}");
+
+            foreach (var item in voetbal)
+            {
+                foreach (var speler in item)
+                {
+                    Console.Write(speler + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
