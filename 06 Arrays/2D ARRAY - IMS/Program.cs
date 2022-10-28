f20850239659e2eaa51d1ff5bf41f79b7c323dcd
Elke Boonen <u0116591@thomasmore.be>
2022-10-28 09:38:11 +0200

    IMS 28/10

---
+++ 06 Arrays/2D ARRAY - IMS/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _2D_ARRAY___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*****************************************/
+            /*********** 2D ARRAY AANMAKEN ***********/
+            /*****************************************/
+
+            int[,] ints = new int[2, 4];
+            //0 0 0 0
+            //0 0 0 0
+
+            string[,] strings = {   { "jan", "piet"} , 
+                                    { "joris", "korneel"},
+                                    { "marie", "jozefien"} };
+            //jan piet
+            //joris korneel
+            //marie jozefien
+
+            /*****************************************/
+            /*********** 2D ARRAY ELEMENTEN **********/
+            /*****************************************/
+
+            //waarde voor , = rij
+            //waarde na , = kolom
+
+            //0 4 0 3
+            //0 1 0 0
+            ints[0, 3] = 3;
+            ints[0, 1] = 4;
+            ints[1, 1] = 1;
+
+            //marie vervangen door maria
+            Console.WriteLine(strings[2, 0]);
+            strings[2, 0] = "maria";
+            Console.WriteLine(strings[2, 0]);
+
+            /*****************************************/
+            /*********** 2D ARRAY OVERLOPEN **********/
+            /*****************************************/
+
+            for (int i = 0; i < ints.GetLength(0); i++) //lopen over de rijen
+            {
+                for (int j = 0; j < ints.GetLength(1); j++) //lopen over de kolommen
+                {
+                    Console.Write(ints[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //je kan een foreach gebruiken als de dimensies niet belangrijk zijn!
+            int sum = 0;
+            foreach (var item in ints)
+            {
+                sum += item;
+            }
+            Console.WriteLine("De som is: " + sum);
+        }
+    }
+}
---
+++ 06 Arrays/RAPPORT - IMS/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RAPPORT___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef aantal leerlingen in: ");
+            int lln = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geef aantal toetsen in: ");
+            int toetsen = Convert.ToInt32(Console.ReadLine());
+
+            int[,] rapport = new int[lln, toetsen];
+
+            for (int i = 0; i < lln; i++)
+            {
+                for (int j = 0; j < toetsen; j++)
+                {
+                    Console.Write($"Cijfer toets {j+1} van leerling {i+1}: ");
+                    rapport[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            //gemiddelde per leerling
+            for (int i = 0; i < lln; i++) //rapport.GetLength(0)
+            {
+                int sum = 0;
+                for (int j = 0; j < toetsen; j++) //rapport.GetLength(1)
+                {
+                    sum += rapport[i, j];
+                }
+                Console.WriteLine($"Gemiddelde voor leerling {i} is {sum/toetsen}");
+            }
+
+            //gemiddelde per toets
+            for (int i = 0; i < toetsen; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < lln; j++)
+                {
+                    sum += rapport[j,i];
+                }
+                Console.WriteLine($"Gemiddelde voor toets {i} is {sum / lln}");
+            }
+
+        }
+    }
+}
