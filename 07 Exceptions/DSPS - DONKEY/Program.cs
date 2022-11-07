93d25b6142cd53f264c81ef721dd853c27cd94ee
Elke Boonen <u0116591@thomasmore.be>
2022-11-07 14:02:02 +0100

    DSPS 7/11

---
+++ 07 Exceptions/DSPS - DONKEY/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DSPS___DONKEY
+{
+    internal class Program
+    {
+        /*
+        The goal of the project is to read in a file donkey.txt and to find all words 
+        (in a unique sequence) that start and end with a given character. 
+        If a non alphabetic character was entered you output 'crazy output'! 
+
+        input
+        d y
+        output
+        day delivery difficulty donkey
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
