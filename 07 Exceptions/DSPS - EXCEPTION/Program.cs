1ea7f7d0fd911ff793671d34846f06feab20c70d
Elke Boonen <u0116591@thomasmore.be>
2022-11-07 12:39:03 +0100

    DSPS 7/11

---
+++ 07 Exceptions/DSPS - EXCEPTION/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace DSPS___EXCEPTION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //NEW WAY --> WORKING WITH EXCEPTION HANDLING
+            try
+            {
+                Console.Write("Enter filename: ");
+                string file = Console.ReadLine();
+                Console.Write("Enter a linenumber: ");
+                int linenr = Convert.ToInt32(Console.ReadLine());
+                string[] text = File.ReadAllLines(file);
+                Console.WriteLine(text[linenr]);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File was not found!");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Linenumber was not valid for this file!");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Linenumber was not a correct number!");
+            }
+            //catch (OverflowException ex)
+            //{
+            //    Console.WriteLine("Linenumber was too big!");
+            //}
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+          
+
+
+
+
+            //OLD WAY OF DOING THINGS
+            /*Console.Write("Enter filename: ");
+            string file = Console.ReadLine();
+
+            Console.Write("Enter a linenumber: ");
+            int linenr;
+            if (!Int32.TryParse(Console.ReadLine(), out linenr))
+            {
+                Console.WriteLine("Not a valid linenumber! We stop!");
+                return;
+            }
+
+            if (File.Exists(file))
+            {
+                string[] text = File.ReadAllLines(file);
+                if (linenr > text.Length)
+                {
+                    Console.WriteLine("Linenumber too big! We stop!");
+                    return;
+                }
+                Console.WriteLine(text[linenr]);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid file! We stop!");
+                return;
+            }
+            */
+        }
+    }
+}
---
+++ 07 Exceptions/DSPS - VOWELS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DSPS___VOWELS
+{
+    internal class Program
+    {
+        /*
+         We want to find the position of every vowel in a string.
+
+            input: Hello
+            output: 2 5
+
+            input: ABC
+            output: 1
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string input = Console.ReadLine().ToLower(); //convert to lowercase!
+            //char[] vowels = { 'a', 'e', 'u', 'i', 'o' };
+
+            //https://learn.microsoft.com/en-us/visualstudio/mac/keyboard-shortcuts?view=vsmac-2022
+            //debugging keys for Mac!!!
+            for (int i = 0; i > input.Length; i++)
+            {
+                //if (vowels.Contains(input[i])
+                //if (input[i]=='a' || input[i] == 'e' || input[i] == 'u' || //etc)
+                if ("aeuio".Contains(input[i]))
+                {
+                    Console.Write($"{i + 1} ");
+                }
+            }
+        }
+    }
+}
