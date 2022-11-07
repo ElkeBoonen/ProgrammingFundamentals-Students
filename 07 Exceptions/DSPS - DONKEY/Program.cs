38f3bab104bb36ca4552b91697bc4233adce77a9
Elke Boonen <u0116591@thomasmore.be>
2022-11-07 14:55:17 +0100

    DSPS 7/11

--- 07 Exceptions/DSPS - DONKEY/Program.cs
+++ 07 Exceptions/DSPS - DONKEY/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DSPS___DONKEY
 {
@@ -16,6 +19,56 @@ namespace DSPS___DONKEY
          */
         static void Main(string[] args)
         {
+            //d y --> read this
+            //012 -> indices
+            string chars = Console.ReadLine().Trim(); //removing spaces before and after
+            char start = chars[0]; //chars[1] --> space
+            char end = chars[2];
+
+            string filename = Console.ReadLine();
+
+            //regex from https://regexr.com/ --> \bd[a-z]*y\b
+            //Regex regex = new Regex(@"\b" + start + "[a-z]*" + end + @"\b");
+            Regex regex = new Regex($"\\b{start}[a-z]*{end}\\b");
+
+
+            string unique = "";
+            MatchCollection matches = regex.Matches(File.ReadAllText(filename));
+            foreach (Match match in matches)
+            {
+                if (!unique.Contains(match.Value))
+                {
+                    unique += match.Value + ":";
+                }
+            }
+
+            string[] words = unique.Split(':');
+            Array.Sort(words);
+            Console.WriteLine(String.Join(" ",words));
+
+            //if you don't have a split
+            regex = new Regex(":");
+            matches = regex.Matches(unique);
+
+            words = new string[matches.Count];
+            string word = "";
+            int index = 0;
+            foreach (var item in unique)
+            {
+                if (item != ':')
+                {
+                    word += item;
+                }
+                else
+                {
+                    words[index] = word;
+                    index++;
+                    word = "";
+                }
+            }
+            Array.Sort(words);
+            Console.WriteLine(String.Join(" ", words));
+
         }
     }
 }
